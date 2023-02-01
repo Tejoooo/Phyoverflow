493b8eba552f435c04a52904629efcd979d3a589
praveen <cbsrirangapraveen.2004@gmail.com>
2023-02-01 12:24:07 +0530

    login page created and function also modified

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.models import User,auth
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 # Create your views here.
 
 
 def index(request):
     return render(request,'index.html')
+
+def login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        try:
+            user = User.objects.filter(email = email)
+        except:
+            messages.error(request,"User doesnt exist!")
+            return redirect('login')
+        user = authenticate(request,email=email,password=password)
+        if user is not None:
+            return redirect('index')
+        else:
+            messages.error(request,"User Credentials doesnot matched")
+            return redirect('login')
+    return render(request,'login.html')
